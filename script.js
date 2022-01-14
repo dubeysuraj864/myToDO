3a8b25276c483e94180af7c102caf8b38d6148a9
Suraj Dubey <dubeysuraj864@gmail.com>
2022-01-14 21:53:46 +0530

    1

---
+++ script.js
@@ -0,0 +1,17 @@
+// window.alert("Hello javapoint")
+// alert("hello");
+
+//create a "close" button and append it to each list item
+var myNodelist = document.getElementsByTagName('LI');
+var i;
+for(i =0; i< myNodelist.length;i++){
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    myNodelist[i].appendChild(span);
+}
+
+
+//
+
